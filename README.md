7c992ee1e7ca919f51f05b23594b987658d2c4d6
Stefan Nicetin <niketic95@gmail.com>
2025-08-12 00:38:45 +0200

    Add build and sdl
    - Proper build system using build.zig
    - Fix SDL3 nod showing up on wayland
    - Link in Chip8 spec

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# chip8_emulator
+# Zig chip8 interpreter
+----
+Intro project to learn zig [specs](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
---
+++ src/main.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("SDL3/SDL.h");
+});
+
+const log = std.log;
+
+pub fn main() !void {
+    if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
+        log.err("SDL_Init: {s}\n", .{c.SDL_GetError()});
+    }
+    defer c.SDL_Quit();
+
+    const window = c.SDL_CreateWindow("Chip8 EMU", 640, 480, 0);
+    if (window == null) {
+        log.err("SDL_CreateWindow: {s}", .{c.SDL_GetError()});
+    }
+    defer c.SDL_DestroyWindow(window);
+
+    log.info("SDLRenderers", .{});
+
+    for (0..@intCast(c.SDL_GetNumRenderDrivers())) |idx| {
+        log.info("RenderDriver: {s}", .{c.SDL_GetRenderDriver(@intCast(idx)).?});
+    }
+
+    const renderer = c.SDL_CreateRenderer(window, null);
+    defer c.SDL_DestroyRenderer(renderer);
+
+    if (renderer == null) {
+        log.err("SDL_CreateRenderer: {s}", .{c.SDL_GetError()});
+    }
+
+    if (!c.SDL_SetRenderDrawColor(renderer, 255, 0, 0, c.SDL_ALPHA_OPAQUE))
+        log.err("SDL_SetRendererDrawColor: {s}", .{c.SDL_GetError()});
+
+    if (!c.SDL_RenderClear(renderer))
+        log.err("SDL_RenderClear: {s}", .{c.SDL_GetError()});
+
+    if (!c.SDL_RenderPresent(renderer))
+        log.err("SDL_RenderPresent: {s}", .{c.SDL_GetError()});
+
+    // var alt: u1 = undefined;
+    // var ctrl: u1 = undefined;
+    var mod: struct {
+        alt: u1,
+        ctrl: u1,
+    } = .{ .alt = 0, .ctrl = 0 };
+
+    var event: c.SDL_Event = undefined;
+    out: while (true) {
+        while (c.SDL_PollEvent(&event)) {
+            if (event.type == c.SDL_EVENT_KEY_UP) {
+                switch (event.key.scancode) {
+                    c.SDL_SCANCODE_LCTRL, c.SDL_SCANCODE_RCTRL => mod.ctrl = 0,
+                    c.SDL_SCANCODE_LALT, c.SDL_SCANCODE_RALT => mod.alt = 0,
+                    else => {},
+                }
+            }
+            if (event.type == c.SDL_EVENT_KEY_DOWN) {
+                switch (event.key.scancode) {
+                    c.SDL_SCANCODE_LCTRL, c.SDL_SCANCODE_RCTRL => mod.ctrl = 1,
+                    c.SDL_SCANCODE_LALT, c.SDL_SCANCODE_RALT => mod.alt = 1,
+                    c.SDL_SCANCODE_F4 => {
+                        if (mod.alt == 1)
+                            break :out;
+                    },
+                    else => {},
+                }
+            }
+        }
+    }
+}
